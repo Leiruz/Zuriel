32909e164c12e30637c700427fe079b89bf5cb84
Zuriel Shanley Tanyory <s10193642@connect.np.edu.sg>
2023-07-06 18:54:38 +0800

    update animation

---
+++ js/animationexit.js
@@ -0,0 +1,11 @@
+const animateCSS = (element, animation, prefix = 'animate__') => {
+    const animationName = `${prefix}${animation}`;
+    const node = document.querySelector(element);
+
+    node.classList.add(`${prefix}animated`, animationName);
+
+    node.addEventListener('animationend', () => {
+        location.replace("./zuriel")
+    });
+
+};
